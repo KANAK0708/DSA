711fb90ceb89998982a586ad07b73cb679600d4f
KANAK <gargkanak802@gmail.com>
2026-07-21 23:20:08 +0530

    Time: 1 ms (75.56%) | Memory: 10.9 MB (41.79%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+         int n=s.length();
+         vector<int>count(26,0);
+         int l=0;
+         int maxlen=0;
+         int maxf=0;
+
+         for(int r=0;r<n;r++){
+            count[s[r] - 'A']++;
+
+
+            maxf=max(maxf,count[s[r]-'A']);
+
+            if((r-l+1)-maxf>k){
+                count[s[l]-'A']--;
+                l++;
+            }
+            maxlen=max(maxlen,r-l+1);
+         }
+         return maxlen;
+    }
+};
