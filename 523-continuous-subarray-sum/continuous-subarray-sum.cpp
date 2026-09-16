288e4e10811afe443196dfd932385d7f0689d2c6
KANAK <gargkanak802@gmail.com>
2026-09-16 23:50:59 +0530

    Time: 70 ms (93.85%) | Memory: 146.3 MB (57.53%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> remainderMap;
+        
+      
+        remainderMap[0] = -1;
+        
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            currentSum += nums[i];
+            
+           
+            int remainder = currentSum % k;
+            
+            
+            if (remainderMap.find(remainder) != remainderMap.end()) {
+                
+                if (i - remainderMap[remainder] > 1) {
+                    return true;
+                }
+            } else {
+               
+                remainderMap[remainder] = i;
+            }
+        }
+        
+        return false;
+    }
+};
